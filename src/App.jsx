0d7ec99d2ce0bd4779c2ab988dd3b08a3fbe2583
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-05 15:46:47 +0300

    minor style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import { Bar } from "react-chartjs-2";
@@ -24,7 +24,7 @@ ChartJS.register(
 );
 
 import { Line } from "react-chartjs-2";
-import MainNav from './components/mainNav';
+import MainNav from "./components/mainNav";
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -34,7 +34,6 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-
 
 function App() {
   const codeCountry = {
@@ -224,10 +223,9 @@ function App() {
     YEM: "Yemen",
     ZAF: "South Africa",
     ZMB: "Zambia",
-    ZWE: "Zimbabwe"
+    ZWE: "Zimbabwe",
   };
-  
-  
+
   const [data, setData] = useState([]);
   const [country, setCountry] = useState("ALL");
   const [countries, setCountries] = useState([]);
@@ -389,100 +387,100 @@ function App() {
 
   return (
     <>
-    <MainNav />
-    <div>
-    <h1>
-        Dataset name: Pilot top-down CO2 Budget constrained by the v10 OCO-2 MIP
-        Version 1.0
-      </h1>
+      <MainNav />
+      <div id="dashboard">
+        <h3>
+          Dataset name: Pilot top-down CO2 Budget constrained by the v10 OCO-2
+          MIP Version 1.0
+        </h3>
 
-      <select
-        name="country"
-        id="country"
-        value={country}
-        onChange={(event) => setCountry(event.target.value)}
-      >
-        <option value="ALL">ALL</option>
-        {countries.map((code, index) => {
-          return (
-            <option value={code} key={index}>
-              {code}
-            </option>
-          );
-        })}
-      </select>
-      <br />
-      <select
-        name="experiment"
-        id="experiment"
-        value={experiment}
-        onChange={(event) => setExperiment(event.target.value)}
-      >
-        <option value="ALL">ALL</option>
-        <option value="IS">IS</option>
-        <option value="LNLG">LNLG</option>
-        <option value="LNLGIS">LNLGIS</option>
-        <option value="LNLGOGIS">LNLGOGIS</option>
-      </select>
-      <div>
-        <h2>Raw Data</h2>
-        {data.length > 0 && (
-          <table>
-            <thead>
-              <tr>
-                {Object.keys(data[0]).map((key, index) => {
-                  if (experiment === "ALL") {
-                    return <th key={index}>{key}</th>;
-                  } else {
-                    const words = key.split(/\s+/);
-                    if (
-                      words.includes(experiment) ||
-                      key === "Alpha 3 Code" ||
-                      key === "Year"
-                    ) {
-                      return <th key={index}>{key}</th>;
-                    }
-                  }
-                })}
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                  {Object.entries(row).map(([property, value], colIndex) => {
+        <select
+          name="country"
+          id="country"
+          value={country}
+          onChange={(event) => setCountry(event.target.value)}
+        >
+          <option value="ALL">ALL</option>
+          {countries.map((code, index) => {
+            return (
+              <option value={code} key={index}>
+                {code}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <select
+          name="experiment"
+          id="experiment"
+          value={experiment}
+          onChange={(event) => setExperiment(event.target.value)}
+        >
+          <option value="ALL">ALL</option>
+          <option value="IS">IS</option>
+          <option value="LNLG">LNLG</option>
+          <option value="LNLGIS">LNLGIS</option>
+          <option value="LNLGOGIS">LNLGOGIS</option>
+        </select>
+        <div>
+          <h2>Raw Data</h2>
+          {data.length > 0 && (
+            <table>
+              <thead>
+                <tr>
+                  {Object.keys(data[0]).map((key, index) => {
                     if (experiment === "ALL") {
-                      return <td key={colIndex}>{value}</td>;
+                      return <th key={index}>{key}</th>;
                     } else {
-                      const words = property.split(/\s+/); // Split property into words
+                      const words = key.split(/\s+/);
                       if (
                         words.includes(experiment) ||
-                        property === "Alpha 3 Code" ||
-                        property === "Year"
+                        key === "Alpha 3 Code" ||
+                        key === "Year"
                       ) {
-                        return <td key={colIndex}>{value}</td>; // Return the column if the condition matches
+                        return <th key={index}>{key}</th>;
                       }
-                      return null; // Skip columns that don't match
                     }
                   })}
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+              </thead>
+              <tbody>
+                {filteredData.map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {Object.entries(row).map(([property, value], colIndex) => {
+                      if (experiment === "ALL") {
+                        return <td key={colIndex}>{value}</td>;
+                      } else {
+                        const words = property.split(/\s+/); // Split property into words
+                        if (
+                          words.includes(experiment) ||
+                          property === "Alpha 3 Code" ||
+                          property === "Year"
+                        ) {
+                          return <td key={colIndex}>{value}</td>; // Return the column if the condition matches
+                        }
+                        return null; // Skip columns that don't match
+                      }
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+        <div>
+          {country === "ALL" ? (
+            <h2>Please select a country to display charts</h2>
+          ) : (
+            <div>
+              <Bar options={barOptions} data={barData} />
+              <Line options={LineOptions} data={Linedata} />
+            </div>
+          )}
+        </div>
       </div>
-      <div>
-        {country === "ALL" ? (
-          <h2>Please select a country to display charts</h2>
-        ) : (
-          <div>
-            <Bar options={barOptions} data={barData} />
-            <Line options={LineOptions} data={Linedata} />
-          </div>
-        )}
-      </div>
-    </div>
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/mainNav.jsx
+++ src/components/mainNav.jsx
@@ -2,26 +2,24 @@ import dashboard from "../assets/dashboard.svg";
 import team from "../assets/team.svg";
 import references from "../assets/references.svg";
 function MainNav() {
-    return (
-      <>
-        <nav className="navigation">
-          <ul id="links-list">
-            <li>
-              <img src={dashboard} />
-            </li>
-            <li>
-              {" "}
-              <img src={team} />
-            </li>
-            <li>
-              {" "}
-              <img src={references} />
-            </li>
-          </ul>
-        </nav>
-      </>
-    );
-  }
-  
-  export default MainNav;
-  
+  return (
+    <>
+      <nav className="navigation">
+        <ul id="links-list">
+          <li>
+            <img src={dashboard} /> <span>Dashboard</span>
+          </li>
+          <li>
+            <img src={team} />
+            <span>the team</span>
+          </li>
+          <li>
+            <img src={references} /> <span>references</span>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default MainNav;
