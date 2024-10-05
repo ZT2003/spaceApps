39f7c1914e82a74677981a4ac29af7681612408e
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-05 16:01:10 +0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
-import codes from '../public/countryCode';
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -37,6 +36,196 @@ ChartJS.register(
 );
 
 function App() {
+  const codeCountry = {
+    AFG: "Afghanistan",
+    AGO: "Angola",
+    ALB: "Albania",
+    ARE: "United Arab Emirates",
+    ARG: "Argentina",
+    ARM: "Armenia",
+    ATG: "Antigua and Barbuda",
+    AUS: "Australia",
+    AUT: "Austria",
+    AZE: "Azerbaijan",
+    BDI: "Burundi",
+    BEL: "Belgium",
+    BEN: "Benin",
+    BFA: "Burkina Faso",
+    BGD: "Bangladesh",
+    BGR: "Bulgaria",
+    BHS: "Bahamas",
+    BIH: "Bosnia and Herzegovina",
+    BLR: "Belarus",
+    BLZ: "Belize",
+    BOL: "Bolivia",
+    BRA: "Brazil",
+    BRB: "Barbados",
+    BRN: "Brunei",
+    BTN: "Bhutan",
+    BWA: "Botswana",
+    CAF: "Central African Republic",
+    CAN: "Canada",
+    CHE: "Switzerland",
+    CHL: "Chile",
+    CHN: "China",
+    CIV: "Ivory Coast",
+    CMR: "Cameroon",
+    COD: "Congo (Democratic Republic)",
+    COG: "Congo (Republic)",
+    COK: "Cook Islands",
+    COL: "Colombia",
+    COM: "Comoros",
+    CPV: "Cape Verde",
+    CRI: "Costa Rica",
+    CUB: "Cuba",
+    CYP: "Cyprus",
+    CZE: "Czech Republic",
+    DEU: "Germany",
+    DJI: "Djibouti",
+    DMA: "Dominica",
+    DNK: "Denmark",
+    DOM: "Dominican Republic",
+    DZA: "Algeria",
+    ECU: "Ecuador",
+    EGY: "Egypt",
+    ERI: "Eritrea",
+    ESP: "Spain",
+    EST: "Estonia",
+    ETH: "Ethiopia",
+    FIN: "Finland",
+    FJI: "Fiji",
+    FRA: "France",
+    FSM: "Micronesia",
+    GAB: "Gabon",
+    GBR: "United Kingdom",
+    GEO: "Georgia",
+    GHA: "Ghana",
+    GIN: "Guinea",
+    GNB: "Guinea-Bissau",
+    GNQ: "Equatorial Guinea",
+    GRC: "Greece",
+    GRD: "Grenada",
+    GTM: "Guatemala",
+    GUY: "Guyana",
+    HND: "Honduras",
+    HRV: "Croatia",
+    HTI: "Haiti",
+    HUN: "Hungary",
+    IDN: "Indonesia",
+    IND: "India",
+    IRL: "Ireland",
+    IRN: "Iran",
+    IRQ: "Iraq",
+    ISL: "Iceland",
+    ISR: "Israel",
+    ITA: "Italy",
+    JAM: "Jamaica",
+    JOR: "Jordan",
+    JPN: "Japan",
+    KAZ: "Kazakhstan",
+    KEN: "Kenya",
+    KGZ: "Kyrgyzstan",
+    KHM: "Cambodia",
+    KIR: "Kiribati",
+    KNA: "Saint Kitts and Nevis",
+    KOR: "South Korea",
+    KWT: "Kuwait",
+    LAO: "Laos",
+    LBN: "Lebanon",
+    LBR: "Liberia",
+    LBY: "Libya",
+    LCA: "Saint Lucia",
+    LKA: "Sri Lanka",
+    LSO: "Lesotho",
+    LTU: "Lithuania",
+    LVA: "Latvia",
+    MAR: "Morocco",
+    MDA: "Moldova",
+    MDG: "Madagascar",
+    MDV: "Maldives",
+    MEX: "Mexico",
+    MHL: "Marshall Islands",
+    MKD: "North Macedonia",
+    MLI: "Mali",
+    MLT: "Malta",
+    MMR: "Myanmar",
+    MNE: "Montenegro",
+    MNG: "Mongolia",
+    MOZ: "Mozambique",
+    MRT: "Mauritania",
+    MUS: "Mauritius",
+    MWI: "Malawi",
+    MYS: "Malaysia",
+    NAM: "Namibia",
+    NER: "Niger",
+    NGA: "Nigeria",
+    NIC: "Nicaragua",
+    NIU: "Niue",
+    NLD: "Netherlands",
+    NOR: "Norway",
+    NPL: "Nepal",
+    NRU: "Nauru",
+    NZL: "New Zealand",
+    OMN: "Oman",
+    PAK: "Pakistan",
+    PAN: "Panama",
+    PER: "Peru",
+    PHL: "Philippines",
+    PLW: "Palau",
+    PNG: "Papua New Guinea",
+    POL: "Poland",
+    PRK: "North Korea",
+    PRT: "Portugal",
+    PRY: "Paraguay",
+    QAT: "Qatar",
+    ROU: "Romania",
+    RUS: "Russia",
+    RWA: "Rwanda",
+    SAU: "Saudi Arabia",
+    SDN: "Sudan",
+    SEN: "Senegal",
+    SLB: "Solomon Islands",
+    SLE: "Sierra Leone",
+    SLV: "El Salvador",
+    SOM: "Somalia",
+    SRB: "Serbia",
+    SSD: "South Sudan",
+    STP: "Sao Tome and Principe",
+    SUR: "Suriname",
+    SVK: "Slovakia",
+    SVN: "Slovenia",
+    SWE: "Sweden",
+    SWZ: "Eswatini",
+    SYC: "Seychelles",
+    SYR: "Syria",
+    TCD: "Chad",
+    TGO: "Togo",
+    THA: "Thailand",
+    TJK: "Tajikistan",
+    TKM: "Turkmenistan",
+    TLS: "Timor-Leste",
+    TON: "Tonga",
+    TTO: "Trinidad and Tobago",
+    TUN: "Tunisia",
+    TUR: "Turkey",
+    TUV: "Tuvalu",
+    TZA: "Tanzania",
+    UGA: "Uganda",
+    UKR: "Ukraine",
+    URY: "Uruguay",
+    USA: "United States",
+    UZB: "Uzbekistan",
+    VCT: "Saint Vincent and the Grenadines",
+    VEN: "Venezuela",
+    VNM: "Vietnam",
+    VUT: "Vanuatu",
+    WSM: "Samoa",
+    YEM: "Yemen",
+    ZAF: "South Africa",
+    ZMB: "Zambia",
+    ZWE: "Zimbabwe",
+  };
+
   const [data, setData] = useState([]);
   const [country, setCountry] = useState("ALL");
   const [countries, setCountries] = useState([]);
@@ -205,62 +394,6 @@ function App() {
           MIP Version 1.0
         </h3>
 
-<<<<<<< HEAD
-      <select
-        name="country"
-        id="country"
-        value={country}
-        onChange={(event) => setCountry(event.target.value)}
-      >
-        <option value="ALL">ALL</option>
-        {codes.map((obj, index) => {
-          return (
-            <option value={obj.code} key={index}>
-              {obj.country}
-            </option>
-          );
-        })}
-      </select>
-      <br />
-      <select
-        name="experiment"
-        id="experiment"
-        value={experiment}
-        onChange={(event) => setExperiment(event.target.value)}
-      >
-        <option value="ALL">ALL</option>
-        <option value="IS">IS</option>
-        <option value="LNLG">LNLG</option>
-        <option value="LNLGIS">LNLGIS</option>
-        <option value="LNLGOGIS">LNLGOGIS</option>
-      </select>
-      <div>
-        <h2>Raw Data</h2>
-        {data.length > 0 && (
-          <table>
-            <thead>
-              <tr>
-                {Object.keys(data[0]).map((key, index) => {
-                  if (experiment === "ALL") {
-                    return <th key={index}>{key}</th>;
-                  } else {
-                    const words = key.split(/\s+/);
-                    if (
-                      words.includes(experiment) ||
-                      key === "Alpha 3 Code" ||
-                      key === "Year"
-                    ) {
-                      return <th key={index}>{key}</th>;
-                    }
-                  }
-                })}
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                  {Object.entries(row).map(([property, value], colIndex) => {
-=======
         <select
           name="country"
           id="country"
@@ -296,7 +429,6 @@ function App() {
               <thead>
                 <tr>
                   {Object.keys(data[0]).map((key, index) => {
->>>>>>> 1814ddf379441f6240119f9603b671d4dc18a98b
                     if (experiment === "ALL") {
                       return <th key={index}>{key}</th>;
                     } else {
