59b4425dbda1fe0a34d78d83f68e26e30c54a6b3
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-06 09:21:05 +0300

    about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,23 +4,25 @@ import MainNav from "../components/mainNav";
 function AboutUs() {
   return (
     <>
-    <MainNav />
+      <MainNav />
       <h1> Meet the Team !!</h1>
-      <div className="team-cards-div">
-        <div className="team-card">
-          <img src={tech} alt="" />
-          <h3>Fatima Ali</h3>
-          <p> a programming student in Bahrain Polytechnic</p>
-        </div>
-        <div className="team-card">
-          <img src={tech} alt="" />
-          <h3>Fatima Ali</h3>
-          <p> a programming student in Bahrain Polytechnic</p>
-        </div>
-        <div className="team-card">
-          <img src={tech} alt="" />
-          <h3>Fatima Ali</h3>
-          <p> a programming student in Bahrain Polytechnic</p>
+      <div>
+        <div className="team-cards-div">
+          <div className="team-card">
+            <img src={tech} alt="" />
+            <h3>Fatima Ali</h3>
+            <p> a programming student in Bahrain Polytechnic</p>
+          </div>
+          <div className="team-card">
+            <img src={tech} alt="" />
+            <h3>Fatima Ali</h3>
+            <p> a programming student in Bahrain Polytechnic</p>
+          </div>
+          <div className="team-card">
+            <img src={tech} alt="" />
+            <h3>Fatima Ali</h3>
+            <p> a programming student in Bahrain Polytechnic</p>
+          </div>
         </div>
       </div>
     </>
