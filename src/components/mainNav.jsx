a70da98818a9971205a00e7464cf1ef6192c304f
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-06 11:33:03 +0300

    arabic

--- src/components/mainNav.jsx
+++ src/components/mainNav.jsx
@@ -1,17 +1,23 @@
 import dashboard from "../assets/icon/dashboard.svg";
 import team from "../assets/icon/team.svg";
 import references from "../assets/icon/references.svg";
-import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 
-function MainNav({english, setEnglish}) {
-  
+function MainNav({ english, setEnglish }) {
   return (
     <>
       <nav className="navigation">
         <p className="logo">space technicians</p>
-        {
-          (english)? <button onClick={() => setEnglish(false)}>ع</button> : <button onClick={() => setEnglish(true)}>E</button>
-        }
+        {english ? (
+          <button onClick={() => setEnglish(false)}>ع</button>
+        ) : (
+          <button onClick={() => setEnglish(true)}>E</button>
+        )}
         <ul id="links-list">
           <li>
             <NavLink to="/">
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -181,13 +181,11 @@ function Dash({ english, setEnglish }) {
     <>
       <MainNav english={english} setEnglish={setEnglish} />
 
-      <div id="dashboard">
+      <div id="dashboard" className="arabic">
         {english ? (
           <h1>Explore the yearly emissions of CO2 in your country</h1>
         ) : (
-          <h1 className="arabic-font">
-            استكشف انبعاثات ثاني أكسيد الكربون السنوية في بلدك
-          </h1>
+          <h1>استكشف انبعاثات ثاني أكسيد الكربون السنوية في بلدك</h1>
         )}
 
         <div className="selector-div">
@@ -227,129 +225,140 @@ function Dash({ english, setEnglish }) {
             </div>
           )}
         </div>
-        {
-            (english)? 
-            <div id="info">
-          <h3>How to interpret the chart ?</h3>
-          <ul className="info_list">
-            <li>
-              This barchart contains annual net fluxes of CO₂ on a 1-degree grid
-              for a six-year period (2015-2020)
-            </li>
-            <li>
-              The chart showcase: net carbon exchange (NCE), fossil fuel
-              emissions (FF), and lateral fluxes due to crop trade, wood trade,
-              and river export.
-            </li>
-            <li>
-              Data is provided in units of grams of carbon dioxide per square
-              meter per year (g CO₂/m2/yr)
-            </li>
-          </ul>
-          <br />
-          <br />
-          <h3>important definitions</h3>
-          <ul className="info_list">
-            <li>
-              <b>Greenhouse Gasses (GHGs):</b> gasses in the atmosphere that
-              trap heat from the Sun.
-            </li>
-            <li>
-              <b> Fossil Fuel:</b> Any hydrocarbon (chemical containing only
-              carbon and hydrogen) deposit that can be burned for heat or power,
-              such as petroleum, coal, and natural gas.
-            </li>
-            <li>
-              <b>Carbon Dioxide (CO2): </b>A naturally occurring gas, CO2 is
-              also a byproduct of burning fossil fuels (such as oil, gas, and
-              coal), of burning biomass, of land-use changes (LUCs) and of
-              industrial processes (e.g., cement production). It is the main gas
-              contributing to climate change.
-            </li>
-            <li>
-              <b> GHG Source:</b> Something that releases a greenhouse gas into
-              the atmosphere. For example, the burning of fossil fuels is a
-              source of GHG emissions.
-            </li>
-            <li>
-              <b> GHG Sink:</b> Something that removes a greenhouse gas from the
-              atmosphere. For example, plants—through photosynthesis—transform
-              carbon dioxide in the air into organic matter, which either stays
-              in the plants or is stored in the soils. Thus, plants are a sink
-              for carbon dioxide.
-            </li>
-            <li>
-              <b> Global Carbon Budget:</b> the assessment of global carbon
-              sources and sinks, and the resulting change in atmospheric CO2
-              concentration.
-            </li>
-            <li>
-              <b>Total Carbon Budget:</b> the maximum amount of net global
-              human-caused CO2 emissions that would result in limiting global
-              warming to a given level.
-            </li>
-            <li>
-              <b> Lateral Fluxes:</b> the flow of carbon between different types
-              of sources and sinks (e.g., lateral flow of agricultural products
-              from growth to consumption, riverine transport of soil carbon from
-              land to ocean systems).
-            </li>
-          </ul>
-        </div>
-        :
-        <div id="info">
-          <h3>كيف يمكن قراءة الرسم البياني؟</h3>
-          <ul className="info_list">
-            <li>
-              هذا الرسم البياني يبين الانبعاثات السنوية لثاني أكسيد الكربون على مدى ست سنوات(2015-2020)
-            </li>
-            <li>
-              الرسم يظهر الانبعاثات من عدة مصادر: الأنهار، قطع الأخشاب، والوقود الأحفوري
-            </li>
-            <li>
-              البيانات تظهر بوحدة  قرام من ثاني أكسيد الكربون لكل متر مربع لكل سنة (g CO₂/m2/yr)
-            </li>
-          </ul>
-          <br />
-          <br />
-          <h3>مصطلحات مهمة</h3>
-          <ul className="info_list">
-            <li>
-              <b>الغازات الدفيئة(GHGs):</b> 
-              غازات في الغلاف الجوي تحبس الحرارة القادمة من الشمس
-            </li>
-            <li>
-              <b> الوقود الأحفوري:</b> 
-            أي مركب كيميائي يحتوي على عناصر الكربون والهيدروجين فقط ويتم حرقه للحصول على الطاقة؛ مثل البترول،الفحم، والغاز الطبيعي
-
-            </li>
-            <li>
-              <b>ثاني أكسيد الكربون (CO2): </b>
-              غاز طبيعي، وقد ينتج من حرق الوقود الأحفوري، حرق الكتلة الحيوية، تغيير معالم الأرض، والنشاطات الصناعية (مثل إنتاج السمنت). وهو المسبب الأساسي للاحتباس الحراري
-            </li>
-            <li>
-              <b> مصادر الغازات الدفيئة:</b> 
-              أي شيء يطلق الغازات الدفيئة في الغلاف الجوي؛ مثل حرق الوقودالأحفوري
-            </li>
-            <li>
-              <b> مصرف للغازات الدفيئة:</b> 
-                أي شيء يمتص غاز دفيء من الغلاف الجوي؛ مثل عملية البناء الضوئي التي تقوم بها النباتات، حيث تحول ثاني أكسيد الكربون إلى مواد عضوية تبقى في النبات أو تتخزن في التربة. فالنباتات مصرف لثاني أكسيد الكربون
-            </li>
-            <li>
-              <b> ميزانية الكربون العالمية:</b> 
-              دراسة المصادر والمصارف العالمية للكربون، والتغيرات الناتجة لتركيز ثاني أكسيد الكربون في الغلاف الجوي
-            </li>
-            <li>
-              <b>ميزانية الكربون الكلية:</b> 
-              أكبر مقدار من انبعاثات ثاني أكسيد الكربون الكلية من نشاطات بشرية، والذي سيحدد الاحتباس الحراري لمستوى معين
-            </li>
-            <li>
-              <b> التدفقات الأفقية:</b> 
-              دورة الكربون بين المصادر والمصارف المختلفة، مثل الزراعة من النمو لحد الاستهلاك، ونقل الأنهار للكربون الترابي من اليابسة للمحيط
-            </li>
-          </ul>
-        </div>
-        }
+        {english ? (
+          <div id="info">
+            <h3>How to interpret the chart ?</h3>
+            <ul className="info_list">
+              <li>
+                This barchart contains annual net fluxes of CO₂ on a 1-degree
+                grid for a six-year period (2015-2020)
+              </li>
+              <li>
+                The chart showcase: net carbon exchange (NCE), fossil fuel
+                emissions (FF), and lateral fluxes due to crop trade, wood
+                trade, and river export.
+              </li>
+              <li>
+                Data is provided in units of grams of carbon dioxide per square
+                meter per year (g CO₂/m2/yr)
+              </li>
+            </ul>
+            <br />
+            <br />
+            <h3>important definitions</h3>
+            <ul className="info_list">
+              <li>
+                <b>Greenhouse Gasses (GHGs):</b> gasses in the atmosphere that
+                trap heat from the Sun.
+              </li>
+              <li>
+                <b> Fossil Fuel:</b> Any hydrocarbon (chemical containing only
+                carbon and hydrogen) deposit that can be burned for heat or
+                power, such as petroleum, coal, and natural gas.
+              </li>
+              <li>
+                <b>Carbon Dioxide (CO2): </b>A naturally occurring gas, CO2 is
+                also a byproduct of burning fossil fuels (such as oil, gas, and
+                coal), of burning biomass, of land-use changes (LUCs) and of
+                industrial processes (e.g., cement production). It is the main
+                gas contributing to climate change.
+              </li>
+              <li>
+                <b> GHG Source:</b> Something that releases a greenhouse gas
+                into the atmosphere. For example, the burning of fossil fuels is
+                a source of GHG emissions.
+              </li>
+              <li>
+                <b> GHG Sink:</b> Something that removes a greenhouse gas from
+                the atmosphere. For example, plants—through
+                photosynthesis—transform carbon dioxide in the air into organic
+                matter, which either stays in the plants or is stored in the
+                soils. Thus, plants are a sink for carbon dioxide.
+              </li>
+              <li>
+                <b> Global Carbon Budget:</b> the assessment of global carbon
+                sources and sinks, and the resulting change in atmospheric CO2
+                concentration.
+              </li>
+              <li>
+                <b>Total Carbon Budget:</b> the maximum amount of net global
+                human-caused CO2 emissions that would result in limiting global
+                warming to a given level.
+              </li>
+              <li>
+                <b> Lateral Fluxes:</b> the flow of carbon between different
+                types of sources and sinks (e.g., lateral flow of agricultural
+                products from growth to consumption, riverine transport of soil
+                carbon from land to ocean systems).
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div id="info">
+            <h3>كيف يمكن قراءة الرسم البياني؟</h3>
+            <ul className="info_list">
+              <li>
+                هذا الرسم البياني يبين الانبعاثات السنوية لثاني أكسيد الكربون
+                على مدى ست سنوات(2015-2020)
+              </li>
+              <li>
+                الرسم يظهر الانبعاثات من عدة مصادر: الأنهار، قطع الأخشاب،
+                والوقود الأحفوري
+              </li>
+              <li>
+                البيانات تظهر بوحدة قرام من ثاني أكسيد الكربون لكل متر مربع لكل
+                سنة (g CO₂/m2/yr)
+              </li>
+            </ul>
+            <br />
+            <br />
+            <h3>مصطلحات مهمة</h3>
+            <ul className="info_list">
+              <li>
+                <b>الغازات الدفيئة(GHGs):</b>
+                غازات في الغلاف الجوي تحبس الحرارة القادمة من الشمس
+              </li>
+              <li>
+                <b> الوقود الأحفوري:</b>
+                أي مركب كيميائي يحتوي على عناصر الكربون والهيدروجين فقط ويتم
+                حرقه للحصول على الطاقة؛ مثل البترول،الفحم، والغاز الطبيعي
+              </li>
+              <li>
+                <b>ثاني أكسيد الكربون (CO2): </b>
+                غاز طبيعي، وقد ينتج من حرق الوقود الأحفوري، حرق الكتلة الحيوية،
+                تغيير معالم الأرض، والنشاطات الصناعية (مثل إنتاج السمنت). وهو
+                المسبب الأساسي للاحتباس الحراري
+              </li>
+              <li>
+                <b> مصادر الغازات الدفيئة:</b>
+                أي شيء يطلق الغازات الدفيئة في الغلاف الجوي؛ مثل حرق
+                الوقودالأحفوري
+              </li>
+              <li>
+                <b> مصرف للغازات الدفيئة:</b>
+                أي شيء يمتص غاز دفيء من الغلاف الجوي؛ مثل عملية البناء الضوئي
+                التي تقوم بها النباتات، حيث تحول ثاني أكسيد الكربون إلى مواد
+                عضوية تبقى في النبات أو تتخزن في التربة. فالنباتات مصرف لثاني
+                أكسيد الكربون
+              </li>
+              <li>
+                <b> ميزانية الكربون العالمية:</b>
+                دراسة المصادر والمصارف العالمية للكربون، والتغيرات الناتجة
+                لتركيز ثاني أكسيد الكربون في الغلاف الجوي
+              </li>
+              <li>
+                <b>ميزانية الكربون الكلية:</b>
+                أكبر مقدار من انبعاثات ثاني أكسيد الكربون الكلية من نشاطات
+                بشرية، والذي سيحدد الاحتباس الحراري لمستوى معين
+              </li>
+              <li>
+                <b> التدفقات الأفقية:</b>
+                دورة الكربون بين المصادر والمصارف المختلفة، مثل الزراعة من النمو
+                لحد الاستهلاك، ونقل الأنهار للكربون الترابي من اليابسة للمحيط
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
